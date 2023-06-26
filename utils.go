a8f23018b89dba2ac6da4535834546309994ce44
Kosala Tennakoon <kosala.me@gmail.com>
2023-06-26 11:32:10 +0530

    added reading underlying value of pointers

--- utils.go
+++ utils.go
@@ -1,9 +1,17 @@
 package validator
 
-import "reflect"
+import (
+	"reflect"
+)
 
+// isSlice checks whether the given unpacker is a slice or an array.
 func isSlice(data interface{}) bool {
 	value := reflect.ValueOf(data)
+
+	// if data is a pointer, get the underlying value
+	if value.Kind() == reflect.Ptr {
+		value = value.Elem()
+	}
 
 	// Check if the underlying type of the value is an array
 	return value.Kind() == reflect.Array || value.Kind() == reflect.Slice
